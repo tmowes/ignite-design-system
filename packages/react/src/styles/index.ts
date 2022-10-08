629f2212504f3a1fcfc45d2afaeb72bed2963422
tmowes <t-mowes@hotmail.com>
2022-10-08 19:21:05 -0300

    update packages names

--- packages/react/src/styles/index.ts
+++ packages/react/src/styles/index.ts
@@ -6,7 +6,7 @@ import {
   radii,
   space,
   lineHeights,
-} from '@ignite-ui/tokens'
+} from '@tmowes-ignite-ui/tokens'
 import { createStitches, defaultThemeMap } from '@stitches/react'
 
 export const {
